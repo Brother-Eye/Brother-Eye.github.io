bda408c9532b83f2ee4feb34e35c1e2c8b58d894
Diego Souza <diego.rosse.souza@gmail.com>
2020-04-30 16:56:55 -0300

    alterando javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const linkElement = document.getElementById("ar-link");
 linkElement.addEventListener("message", function(event){
   if (event.data == "apple_ar_quicklook_button_tapped") {
-    window.location.href = "teladolink.html"
+    window.open("https://www.google.com.br")
   }
 }, false)
