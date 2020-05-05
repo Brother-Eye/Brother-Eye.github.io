bc14a81c2c968f556d3e2c58d7e6bb676c83d147
Diego Souza <diego.rosse.souza@gmail.com>
2020-05-05 11:02:23 -0300

    alterações de teste

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const linkElement = document.getElementById("ar-link");
 linkElement.addEventListener("message", function(event){
   console.log("message sent");
+  window.open("https://www.google.com.br")
   if (event.data == "apple_ar_quicklook_button_tapped") {
     console.log("data correct");
-    window.open("https://www.google.com.br")
   }
 }, false)
