e2d42c613fc564febfcd30ea839d9689cb596e6d
Diego Souza <diego.rosse.souza@gmail.com>
2020-05-07 09:46:59 -0300

    event log

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const linkElement = document.getElementById("ar-link");
 linkElement.addEventListener("message", function(event){
   console.log("message sent");
   window.open("https://www.google.com.br")
+  console.log(event);
   if (event.data == "apple_ar_quicklook_button_tapped") {
     console.log("data correct");
   }
