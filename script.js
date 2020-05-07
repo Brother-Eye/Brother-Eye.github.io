ec1f4494408dcdfb9e46682ee4cbcfed95f70a94
Diego Souza <diego.rosse.souza@gmail.com>
2020-05-07 10:01:39 -0300

    alteração call to action

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const linkElement = document.getElementById("ar-link");
 linkElement.addEventListener("message", function(event){
-  console.log("message sent");
-  window.open("https://www.google.com.br")
-  console.log(event);
-  if (event.data == "apple_ar_quicklook_button_tapped") {
+  if (event.data == "_apple_ar_quicklook_button_tapped") {
+    window.open("https://www.google.com.br")
     console.log("data correct");
   }
-}, false)
-
-const linkTest = document.getElementById("button-test");
-linkTest.addEventListener("click", function(event){
-  console.log("click test");
-  window.open("teladolink.html")
 }, false)
