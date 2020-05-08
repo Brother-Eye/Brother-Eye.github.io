742122eceea497942461d203a73dedf513610c0a
Diego Souza <diego.rosse.souza@gmail.com>
2020-05-08 15:04:05 -0300

    codigo limpo ultimo

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const linkElement = document.getElementById("ar-link");
 linkElement.addEventListener("message", function(event){
   console.log(event);
-  if (event.data == "_apple_ar_quicklook_button_tapped") {
-    window.open("https://www.google.com.br")
-
-    console.log("message sent");
-    window.open("https://www.google.com.br")
-    console.log(event);
-    if (event.data == "apple_ar_quicklook_button_tapped") {
-      console.log("data correct");
-    }
-  }, false)
+  window.open("https://www.google.com");
+  if (event.data == "apple_ar_quicklook_button_tapped") {
+    console.log("data correct");
+  }
+}, false);
 
   const linkTest = document.getElementById("button-test");
   linkTest.addEventListener("click", function(event){
     console.log("click test");
-    window.open("teladolink.html")
-  }, false)
+    window.open("teladolink.html");
+  }, false);
