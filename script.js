30abc6f7831d8c3fb272f40dcb624a1b35b17bd0
Diego Souza <diego.rosse.souza@gmail.com>
2020-06-08 15:50:06 -0300

    revert Three.js

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function(){
+  $("#ar-link").click();
+  console.log("DOM ready");
+});
+const linkElement = document.getElementById("ar-link");
+linkElement.addEventListener("message", function(event){
+  console.log(event);
+  window.open("https://www.google.com");
+  if (event.data == "_apple_ar_quicklook_button_tapped") {
+    console.log("data correct");
+  }
+}, false);
+
+  const linkTest = document.getElementById("button-test");
+  linkTest.addEventListener("click", function(event){
+    console.log("click test");
+    window.open("teladolink.html");
+  }, false);
